0efc1123eac1173dd003390e2a04de105f472d69
Mykola Pavlov <me@nikolaypavlov.com>
2016-04-09 13:19:34 +0300

    Usage counter added

--- experienceReplay.py
+++ experienceReplay.py
@@ -3,15 +3,32 @@ import numpy as np
 class ExperienceReplay:
     """Stores all trajectories during the life of the agent"""
 
-    def __init__(self):
+    def __init__(self, min_usages=8, clean_at=100):
         self.experience = []
+        self.min_usages = min_usages
+        self.clean_at = clean_at
 
     def append(self, state, action, reward, next_state):
-        self.experience.append((state, action, reward, next_state))
+        self.experience.append((state, action, reward, next_state, 0))
 
     def mini_batch(self, size):
-        idx = np.random.choice(range(len(self.experience)), size)
-        return [self.experience[n] for n in idx]
+        idx = np.random.choice(range(len(self.experience)), size, replace=False)
+        batch = [None] * size
+        removelist = []
+        for i, n in enumerate(idx):
+            state, action, reward, new_action, num = self.experience[n]
+            batch[i] = (state, action, reward, new_action)
+            num += 1
+            if num >= self.min_usages:
+                removelist.append(n)
+
+            self.experience[n] = (state, action, reward, new_action, num)
+
+        if self.getSize() >= self.clean_at:
+            for i in sorted(removelist, reverse=True):
+                del self.experience[i]
+
+        return batch
 
     def getSize(self):
         return len(self.experience)
