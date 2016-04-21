313300f07669fbf33ad06876caeaba2c0f0e4171
Mykola Pavlov <me@nikolaypavlov.com>
2016-04-21 10:49:16 +0300

    Prioritized experience replay implementation

--- experienceReplay.py
+++ experienceReplay.py
@@ -1,4 +1,5 @@
 import numpy as np
+from blist import sortedset
 
 class ExperienceReplay:
     """Stores all trajectories during the life of the agent"""
@@ -32,3 +33,35 @@ class ExperienceReplay:
 
     def getSize(self):
         return len(self.experience)
+
+class PrioritizedExperienceReplay():
+    def __init__(self, alpha=0, min_usages=8, clean_at=1000):
+        self.sum = 0
+        self._alpha = alpha
+        self.min_usages = min_usages
+        self.clean_at = clean_at
+        self.experience = sortedset(key=lambda x: x[0])
+
+    def append(self, state, action, reward, next_state, count=0, td_err=None):
+        if count < self.min_usages or self.getSize() < self.clean_at:
+            priority = (abs(td_err) + np.finfo(np.float32).eps) ** self.alpha
+            transition = (priority, (state, action, reward, next_state), count)
+            self.experience.add(transition)
+            self.sum += priority
+
+    def mini_batch(self, size):
+        space = np.linspace(0, self.sum, size + 1)
+        ranges = zip(space, space[1:])
+        batch = [None] * size
+        for i, (left, right) in enumerate(ranges):
+            start = self.experience.bisect_left((left, None, None))
+            end = self.experience.bisect_right((right, None, None))
+            sample_idx = (np.random.choice(np.arange(start, end), 1, replace=False))
+            priority, (state, action, reward, next_state), count = self.experience.pop(sample_idx)
+            self.sum -= priority
+            batch[i] = ((state, action, reward, next_state), count + 1)
+
+        return batch
+
+    def getSize(self):
+        return len(self.replay)
