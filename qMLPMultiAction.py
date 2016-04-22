7e29faac14941b6d0d4c02a8e4b5af3baab9b160
Mykola Pavlov <me@nikolaypavlov.com>
2016-04-22 13:21:49 +0300

    PrioritizedExperienceReplay used

--- qMLPMultiAction.py
+++ qMLPMultiAction.py
@@ -1,6 +1,6 @@
 from F9utils import RLAgent
 from F9utils import Snapshot
-from experienceReplay import ExperienceReplay
+from experienceReplay import PrioritizedExperienceReplay
 import numpy as np
 import random
 import logging
@@ -14,11 +14,11 @@ from lasagne.regularization import regularize_network_params, l2
 from lasagne.objectives import squared_error
 
 # Number of units in the hidden (recurrent) layer
-N_HIDDEN = 512
+N_HIDDEN = 128
 # Weight decay
 DECAY = 1.0e-6
 # Mini-batch size
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 # Discount factor gamma
 GAMMA = 0.99
 # Epsilon-greedy policy parameters
@@ -27,12 +27,16 @@ EPS = 0.1
 # Synchronise target model afterwhile
 SYNC_TARGET_MODEL = 5000
 # Wait till replay accumulate some experience than start learning
-MIN_REPLAY_SIZE = 1000
+MIN_REPLAY_SIZE = 2048
 # Log file path
 LOG_FILE = "output.log"
 # Take snapshots
 SNAPSHOT_EVERY = 10000
 SNAPSHOT_PREFIX = 'snapshots/qmlp'
+# How much prioritization to use
+ALPHA = 0.6
+# Bias annealing term
+BETA = 0.6
 
 class QMLPMultiActionAgent(RLAgent):
     """Q-Learning agent with MLP function approximation"""
@@ -45,12 +49,12 @@ class QMLPMultiActionAgent(RLAgent):
         self.explorationProb = EPS0
         self.numIters = 0
         self.max_iters = max_iters
-        self.start_learning = False
         self.total_reward = 0
         self.gamma = GAMMA
-        self.replay = ExperienceReplay(clean_at=MIN_REPLAY_SIZE)
+        self.replay = PrioritizedExperienceReplay(alpha=ALPHA, capacity=MIN_REPLAY_SIZE, batch_size=BATCH_SIZE)
+        self.stats = {}
 
-        logging.basicConfig(filename=LOG_FILE, level=logging.DEBUG)
+        logging.basicConfig(filename=LOG_FILE, level=logging.INFO)
         self.snapshot = Snapshot(SNAPSHOT_PREFIX)
 
         self.model, self.train, self.predict = self._create_network()
@@ -92,10 +96,10 @@ class QMLPMultiActionAgent(RLAgent):
         network_output = lasagne.layers.get_output(net)
         new_target_values = target_values * maxQ_mask + network_output * mask
 
-        cost = squared_error(network_output, new_target_values).mean()
+        err = squared_error(network_output, new_target_values)
 
         # Add regularization penalty
-        cost += regularize_network_params(net, l2) * DECAY
+        cost = err.mean() + regularize_network_params(net, l2) * DECAY
 
         # Retrieve all parameters from the network
         all_params = lasagne.layers.get_all_params(net, trainable=True)
@@ -105,7 +109,7 @@ class QMLPMultiActionAgent(RLAgent):
 
         # Theano functions for training and computing cost
         print("Compiling functions ...")
-        train = theano.function([input_var, target_values], [cost, new_target_values, network_output, maxQ_idx], updates=updates)
+        train = theano.function([input_var, target_values], [cost, new_target_values, network_output, err.mean(1), maxQ_idx], updates=updates)
         predict = theano.function([input_var], lasagne.layers.get_output(net))
 
         return net, train, predict
@@ -132,21 +136,19 @@ class QMLPMultiActionAgent(RLAgent):
     def provideFeedback(self, state, action, reward, new_state):
         self.replay.append(state, action, reward, new_state) # Put new data to experience replay
         self.total_reward += reward
-        if self.replay.getSize() > MIN_REPLAY_SIZE:
-            self.start_learning = True
 
-        if self.start_learning:
-            batch = self.replay.mini_batch(BATCH_SIZE)
+        if self.replay.getSize() >= MIN_REPLAY_SIZE:
+            # totalPriority = self.replay.sum
+            batch = self.replay.mini_batch()
             target = np.zeros((BATCH_SIZE, self.actionsNum), dtype=np.float64)
             features = np.zeros((BATCH_SIZE, self.featuresNum), dtype=np.float64)
 
             for i, sars in enumerate(batch):
-                b_state, b_action, b_reward, b_new_state = sars
+                ((b_state, b_action, b_reward, b_new_state), count, _) = sars
                 features[i] = self.featureExtractor(b_state)
 
                 if self.isTerminalState(b_new_state):
-                    target[i].fill(b_reward - 1)
-                    target[i][np.random.randint(0, self.actionsNum)] = b_reward
+                    target[i].fill(b_reward)
                 else:
                     # Double Q-learning target
                     act = np.argmax(self.predict(self.featureExtractor(b_new_state).reshape(-1, self.featuresNum).astype(theano.config.floatX)), 1)
@@ -156,27 +158,36 @@ class QMLPMultiActionAgent(RLAgent):
                     target[i][act] = t[act]
 
             assert(target.shape == (BATCH_SIZE, self.actionsNum))
-            loss, target_val, net_out, maxQ_idx = self.train(features.astype(theano.config.floatX), target.astype(theano.config.floatX))
+            loss, target_val, net_out, err, maxQ_idx = self.train(features.astype(theano.config.floatX), target.astype(theano.config.floatX))
             self.explorationProb -= (EPS0 - EPS) / (self.max_iters - MIN_REPLAY_SIZE)
             assert(np.sum(np.invert(np.isclose(target_val, net_out))) <= BATCH_SIZE)
 
-            logging.info("Iteration: %s Replay: %s TD-err: %s Reward: %s Action %s Epsilon: %s" %\
+            for i, sars in enumerate(batch):
+                ((b_state, b_action, b_reward, b_new_state), count, _) = sars
+                self.replay.append(b_state, b_action, b_reward, b_new_state, count, err[i])
+
+            logging.info("Iteration: %s Replay size: %s TD-err: %s Reward: %s Action %s Epsilon: %s" %\
                             (self.numIters, self.replay.getSize(), loss, reward, action, self.explorationProb))
             logging.debug("maxQ_idx %s" % maxQ_idx)
+            keys, values = zip(*sorted(self.replay.range_stats.items(), key=lambda x: x[0][0]))
+            range_stats = np.array(values) / self.replay.range_count
+            logging.debug("Priority stats %s" % zip(keys, np.diff(range_stats)))
 
-            if self.numIters % SYNC_TARGET_MODEL == 0:
-                self._syncModel()
+        if self.numIters % SYNC_TARGET_MODEL == 0:
+            self._syncModel()
 
-            if self.numIters % SNAPSHOT_EVERY == 0:
-                self._save_snapshot()
+        if self.numIters % SNAPSHOT_EVERY == 0:
+            self._save_snapshot()
+
+    # def _sampling_weight(num, priority, totalPriority):
+    #     weigth = (1.0 / self.maxW) * 1.0 / (num * priority / totalPriority) ** BETA
+    #     if
 
     def _save_snapshot(self):
         snap = {"iter": self.numIters,
                 "epsilon": self.explorationProb,
                 "target_params": lasagne.layers.get_all_param_values(self.targetModel),
-                "params": lasagne.layers.get_all_param_values(self.model),
-                "replay": self.replay,
-                "start_learning": self.start_learning}
+                "params": lasagne.layers.get_all_param_values(self.model)}
         self.snapshot.save(snap, self.numIters)
 
     def _load_snapshot(self):
@@ -186,5 +197,3 @@ class QMLPMultiActionAgent(RLAgent):
             lasagne.layers.set_all_param_values(self.targetModel, snap['target_params'])
             self.numIters = snap["iter"]
             self.explorationProb = snap["epsilon"]
-            self.replay = snap["replay"]
-            self.start_learning = snap["start_learning"]
